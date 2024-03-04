e956c0ec6c610b48d909c2106bbb8f1c161ff1e5
Kirtan-lokadiya <kirtanlokadiya998@gmail.com>
2024-03-04 13:29:01 +0530

    custom form

--- models/addnewscholarships.js
+++ models/addnewscholarships.js
@@ -38,6 +38,10 @@ const ScholarshipSchema = new mongoose.Schema({
   organizationType: {
     type: String,
   },
+  BasedOnScholarship: {
+    type: String,
+    required: true,
+  },
 
   awardAmount: {
     type: Number,
